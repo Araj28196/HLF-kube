fe5ec3ae222243b8486690cbe9fca2eea8ff7e7d
Araj28196 <ckaravindraj@gmail.com>
2024-03-27 15:51:10 +0000

    removed org2 and added peer1 for org1

--- prerequsite/scripts/org1-certs.sh
+++ prerequsite/scripts/org1-certs.sh
@@ -28,7 +28,7 @@ echo 'NodeOUs:
 
 
 fabric-ca-client register --caname ca-org1 --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
-
+fabric-ca-client register --caname ca-org1 --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
 
 
 fabric-ca-client register --caname ca-org1 --id.name user1 --id.secret user1pw --id.type client --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
@@ -41,14 +41,17 @@ fabric-ca-client register --caname ca-org1 --id.name org1admin --id.secret org1a
 
 
 fabric-ca-client enroll -u https://peer0:peer0pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp" --csr.hosts peer0.org1.example.com --csr.hosts  peer0-org1 --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
+fabric-ca-client enroll -u https://peer1:peer1pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp" --csr.hosts peer1.org1.example.com --csr.hosts  peer1-org1 --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
 
 
 
 cp "/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/config.yaml"
+cp "/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/config.yaml"
 
 
 
 fabric-ca-client enroll -u https://peer0:peer0pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls" --enrollment.profile tls --csr.hosts peer0.org1.example.com --csr.hosts  peer0-org1 --csr.hosts ca-org1 --csr.hosts localhost --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
+fabric-ca-client enroll -u https://peer1:peer1pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls" --enrollment.profile tls --csr.hosts peer1.org1.example.com --csr.hosts  peer1-org1 --csr.hosts ca-org1 --csr.hosts localhost --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
 
 
 
@@ -56,6 +59,9 @@ fabric-ca-client enroll -u https://peer0:peer0pw@ca-org1:7054 --caname ca-org1 -
 cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt"
 cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/signcerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.crt"
 cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/keystore/"* "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/server.key"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/signcerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/keystore/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key"
 
 mkdir -p "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts"
 cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts/ca.crt"
@@ -66,6 +72,14 @@ cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.c
 mkdir -p "/organizations/peerOrganizations/org1.example.com/ca"
 cp "/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/msp/cacerts/"* "/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"
 
+mkdir -p "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts/ca.crt"
+
+mkdir -p "/organizations/peerOrganizations/org1.example.com/tlsca"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem"
+
+mkdir -p "/organizations/peerOrganizations/org1.example.com/ca"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/cacerts/"* "/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"
 
 fabric-ca-client enroll -u https://user1:user1pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp" --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
 
